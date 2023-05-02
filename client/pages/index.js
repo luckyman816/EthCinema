ad2bea7f2c83ecf8f55c84ada3974f97961ae5ac
luckyman816 <zhangyunpei816@gmail.com>
2023-05-02 22:59:53 +0530

    givevote and can pay ethers

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,49 @@
-import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import { Inter } from 'next/font/google'
+import abi from '../contracts/CreateVote.json'
+import { ethers } from 'ethers';
+
+import GiveVote from '@/components/GiveVote';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null
+  })
+
+  useEffect(() => {
+    
+    const connectwallet = async () => {
+      const contractAddress = "0x82d3e35bcb9534b95c23896aa8e7c286f6c2ec81";
+      const contractAbi = abi.abi;
+
+      try{
+        const { ethereum } = window;
+
+        if(ethereum){
+          const accounts = await ethereum.request({ method: 'eth_requestAccounts',});
+          
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+          setState({provider, signer, contract});
+        }
+      }
+      catch(error){
+        console.log(error);
+      }
+    }
+
+    connectwallet();
+  }, []);
+  
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-     hello world 
-
-
+    <main className={`${inter.className}`}>
+      <GiveVote state={state} />
     </main>
   )
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_URL = process.env.GOERLI_URL;
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    goerli: {
-      url: GOERLI_URL,
+    sepolia: {
+      url: SEPOLIA_URL,
       accounts: [PRIVATE_KEY]
     },
   },
